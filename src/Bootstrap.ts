f7bbfbb0f7f991fd92f5096ce43cf6c30ffeffbe
Mukhammad Pardaboev <mukhammad.lb.96@gmail.com>
2021-08-04 17:04:37 +0500

    [Commit]: Initial commit

---
+++ src/Bootstrap.ts
@@ -0,0 +1,38 @@
+import Database, { ITodo } from './Database'
+
+export class TodoRepository {
+  private readonly _db = Database
+
+  async save(title: ITodo['title']) {
+    return this._db.create({
+      title,
+      completed: false,
+    })
+  }
+}
+
+export class TodoService {
+  constructor(private readonly _todoRepo: TodoRepository) {}
+
+  async createTodo(title: ITodo['title']) {
+    if (title.length < 6) {
+      throw new Error('Title has to have at least 6 characters')
+    }
+
+    return this._todoRepo.save(title)
+  }
+}
+
+class TodoController {
+  constructor(private readonly _todoService: TodoService) {}
+
+  async store(requestData: string) {
+    return this._todoService.createTodo(requestData)
+  }
+}
+
+const repository = new TodoRepository()
+const service = new TodoService(repository)
+const controller = new TodoController(service)
+
+controller.store('An awesome todo from our http controller!').then(console.log)
